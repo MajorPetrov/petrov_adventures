3c04589709567fcc04d91ed2ba9daa873c34a7b5
MajorPetrov <razdobreevvlad@live.fr>
2017-10-26 23:04:39 +0200

    first working script is ok. The major can kill a character :D

---
+++ items/Rifle.py
@@ -0,0 +1,12 @@
+"""
+This module contains only the class for the rifle item
+"""
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+class Rifle:
+    """
+    Represents the rifle used by the major Petrov (Petrov class)
+    """
+    def __init__(self, ammo):
+        self.ammo = ammo
