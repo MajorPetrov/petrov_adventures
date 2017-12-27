7bd7ab0c2b692740807d656ab94f48f6940bf82f
MajorPetrov <razdobreevvlad@yandex.ru>
2017-12-27 20:57:29 +0100

    Changing all game structure and create main game loop

--- main.py
+++ main.py
@@ -1,21 +1,109 @@
-"""
-Main module for the game The major Petrov against cosmopolitans
-main algorithm for the run the game is implemented here
-"""
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from characters.Character import Character
-from characters.Petrov import Petrov
-#from items.Rifle import Rifle
+"""
+:mod: module
 
-if __name__ == "__main__":
-    MAJOR = Petrov()
-    ENNEMI = Character("a vegan", 10, 10)
-    MAJOR.attack(ENNEMI)
+:author: Vladimir Razdobreev
 
-    if ENNEMI.life_points == 0:
-        del ENNEMI
+:date: 2017, december
 
-# At the end of the script, the shell shows "Game Over" which is in the __del__
-# function of Petrov...
+
+Main game file - the main program loop
+
+Petrov_against_cosmopolitans - platform game
+
+"""
+
+import pygame as pg
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        """
+        Initialize game window
+        """
+        # initialize pygame and create window
+        self.playing = True
+        pg.init()
+        pg.mixer.init()
+        pg.display.set_caption(TITLE)
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        """
+        Start a new game
+        :return: None
+        """
+        self.all_sprites = pg.sprite.Group()
+        g.run()
+
+    def run(self):
+        """
+        Game loop
+        :return: None
+        """
+        while self.playing:
+            # keep loop running at the right speed
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        """
+        Game loop - update
+        :return: None
+        """
+        self.all_sprites.update()
+
+    def events(self):
+        """
+        Game loop - Process input (events)
+        :return: None
+        """
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+
+                self.running = False
+
+    def draw(self):
+        """
+        Game loop - draw / render
+        :return: None
+        """
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+
+        # after drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        """
+        Game splash / start screen
+        :return: None
+        """
+        pass
+
+    def show_go_screen(self):
+        """
+        Game over / continue screen
+        :return: None
+        """
+        pass
+
+
+g = Game()
+g.show_start_screen()
+
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
---
+++ settings.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+:mod: module
+
+:author: Vladimir Razdobreev
+
+:date: 2017, december
+
+
+Game options and setting
+
+"""
+
+TITLE = "Major Petrov"
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
