e5dce809643389c5c94801fd5fd309400d8687d5
MajorPetrov <razdobreevvlad@live.fr>
2017-12-29 23:10:05 +0100

    - new mapping system (replacing list of platforms by an analysis of a text file
    - new camera system which will follow the player
    
    Signed-off-by: MajorPetrov <razdobreevvlad@yandex.ru>

--- settings.py
+++ settings.py
@@ -17,8 +17,8 @@ Petrov_against_cosmopolitans - platform game
 
 # game options / settings
 TITLE = "Major Petrov"
-WIDTH = 1024
-HEIGHT = 720
+WIDTH = 640
+HEIGHT = 480
 FPS = 60
 TILE_SIZE = 32
 GRID_WIDTH = WIDTH / TILE_SIZE
@@ -28,13 +28,6 @@ GRID_HEIGHT = HEIGHT / TILE_SIZE
 PLAYER_ACC = 0.5
 PLAYER_FRICTION = -0.12
 PLAYER_GRAV = 0.5
-
-# Starting platforms
-PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40),
-                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20),
-                 (125, HEIGHT - 350, 100, 20),
-                 (300, 200, 100, 20),
-                 (175, 100, 50, 20)]
 
 # define colors
 WHITE = (255, 255, 255)
